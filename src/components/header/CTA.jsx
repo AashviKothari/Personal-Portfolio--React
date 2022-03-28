ffcb0faa7356c460959475e2a3a03a66f38a469c
Aashvi Kothari <aashvikothari78@gmail.com>
2022-03-28 15:38:50 +0530

    Second Commit

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Cv from '../../assets'
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+      <a href='#'>DownLoad CV</a>
+      <a href='#contact'>Let's Talk</a>
+    </div>
+  )
+}
+
+
+export default CTA
