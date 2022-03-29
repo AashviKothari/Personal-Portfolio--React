739f6e17cf5bca337c3a155ae884693bd508063f
Aashvi Kothari <aashvikothari78@gmail.com>
2022-03-29 13:49:28 +0530

    Third Commit

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {FiHome} from 'react-icons/fi'
+import {FiUserCheck} from 'react-icons/fi'
+import {BiBookBookmark} from 'react-icons/bi'
+import{RiServiceLine} from 'react-icons/ri'
+import {AiFillContacts} from 'react-icons/ai'
 
 const Nav = () => {
   return (
-    <div>
-      Hey!Nav
-    </div>
+    <nav>
+      <a href='#'><FiHome/></a>
+      <a href='#about'><FiUserCheck/></a>
+      <a href='#experience'><BiBookBookmark/></a>
+      <a href='#services'><RiServiceLine/></a>
+      <a href='#contact'><AiFillContacts/></a>
+    </nav>
   )
 }
 
