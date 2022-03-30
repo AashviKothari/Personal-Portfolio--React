af83acd8737538c33930fab34866e3fa8603cc98
Aashvi Kothari <aashvikothari78@gmail.com>
2022-03-30 16:23:30 +0530

    Third Commit

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -11,11 +11,11 @@ const Nav = () => {
   const [activeNav, setActiveNav] = useState('#');
   return (
     <nav>
-      <a href='#' className={activeNav === '#' ? 'active':'' }><FiHome/></a>
+      <a href='#' onClick={()=> setActiveNav('#')} className={activeNav === '#' ? 'active':'' }><FiHome/></a>
       <a href='#about' onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active':''}><FiUserCheck/></a>
-      <a href='#experience'><BiBookBookmark/></a>
-      <a href='#services'><RiServiceLine/></a>
-      <a href='#contact'><AiFillContacts/></a>
+      <a href='#experience' onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active':'' }><BiBookBookmark/></a>
+      <a href='#services' onClick={()=> setActiveNav('#services')} className={activeNav === '#services' ? 'active':'' }><RiServiceLine/></a>
+      <a href='#contact'onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active':'' }><AiFillContacts/></a>
     </nav>
   )
 }
