5430df58527f132f54e09ea33f46dd9383d3061b
Aashvi Kothari <aashvikothari78@gmail.com>
2022-03-30 17:02:08 +0530

    Third Commit

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -15,6 +15,7 @@ const Nav = () => {
       <a href='#about' onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active':''}><FiUserCheck/></a>
       <a href='#experience' onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active':'' }><BiBookBookmark/></a>
       <a href='#services' onClick={()=> setActiveNav('#services')} className={activeNav === '#services' ? 'active':'' }><RiServiceLine/></a>
+     
       <a href='#contact'onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active':'' }><AiFillContacts/></a>
     </nav>
   )
