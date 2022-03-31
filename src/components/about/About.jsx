a562c099a3e5cb436893a7d293761ebd4917ab16
Aashvi Kothari <aashvikothari78@gmail.com>
2022-03-31 16:26:22 +0530

    Third Commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './About.css'
 import {FaAward} from 'react-icons/fa'
 import {FiUsers} from 'react-icons/fi'
+import {MdOutlineFolderSpecial} from 'react-icons/md'
 
 const About = () => {
   return (
@@ -26,16 +27,19 @@ const About = () => {
             <article className='about__card'>
                 <FiUsers className='about__icon'/>
               <h5>Clients</h5>
-              <small>3+ Years Working</small>
+              <small>200+ Clients</small>
             </article>
 
             <article className='about__card'>
-                <FaAward className='about__icon'/>
-              <h5>Experience</h5>
-              <small>3+ Years Working</small>
+                <MdOutlineFolderSpecial className='about__icon'/>
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
             </article>
             
           </div>
+          <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
         </div>
       </div>
     </section>
